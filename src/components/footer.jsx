ebee4940ebee31df28f47375a302e57e81ba813e
Patricio Salazar <psalazar@empire-medical.com>
2024-10-23 23:47:13 -0700

    add target blank attr to link in footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Footer() {
   return (
     <footer className="text-center">
-      <Link href="https://patriciosalazar.ck.page/products/your-next-business-idea-notion-template" className="text-blue-500">Purchase the Notion template here</Link><br />
+      <Link href="https://patriciosalazar.ck.page/products/your-next-business-idea-notion-template" className="text-blue-500" target="_blank">Purchase the Notion template here</Link><br />
       <span>&copy;2024 Your Next Business Idea</span>
     </footer>
   );
