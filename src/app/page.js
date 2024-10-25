11f4932d99beb3c7e93151ecb37a0429e8f740ca
Patricio Salazar <psalazar@empire-medical.com>
2024-10-25 13:09:04 -0700

    added explainer copy to the home page

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,11 @@ import BusinessIdeaTable from "@/components/businessIdeaTable"
 
 export default function Home() {
   return (
-    <BusinessIdeaTable />
+    <>
+      <h1 className="text-base text-center text-balance font-normal mb-12">
+        <span className="font-bold">Have many business and side project ideas?</span> Enter them in the table below to compare them on a scale and make the best decision on Your Next Business Idea. After you click the Calculate Best Idea button, you can <span className="underline">generate a free AI analysis on your top idea</span>.
+      </h1>
+      <BusinessIdeaTable />
+    </>
   );
 }
